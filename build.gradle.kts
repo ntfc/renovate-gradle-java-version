bdf3e4a5a5992bae619de3d61acf150700aa1fe9
Nuno Carvalho <nuno@scalable.capital>
2025-02-05 14:52:23 +0100

    Add more dsl stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val depAVersion = "0.8.1"
+val depBVersion = "1.25.6"
+
 plugins {
     val kotlinVersion = "2.1.10"
 
@@ -12,11 +18,24 @@ group = "me.ntfc"
 java.sourceCompatibility = JavaVersion.VERSION_21
 java.targetCompatibility = JavaVersion.VERSION_21
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.withType<KotlinCompile>().all {
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
 }
 
 tasks.test {
